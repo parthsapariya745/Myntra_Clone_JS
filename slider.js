f2a5a50f384789291a4cfbba02f305b838524d7e
parthsapariya745 <parthsapariyait7@gmail.com>
2025-07-10 15:36:32 +0530

    update

--- slider.js
+++ slider.js
@@ -1,11 +1,17 @@
 let slideWrap1 = document.querySelector("#slideWrap1")
 let dot1 = document.querySelectorAll(".dot1")
+let slides1 = dot1.length
+let cIndex1 = 0
 
 let slideWrap2 = document.querySelector("#slideWrap2")
 let dot2 = document.querySelectorAll(".dot2")
+let slides2 = dot2.length
+let cIndex2 = 0
 
 let slideWrap3 = document.querySelector("#slideWrap3")
 let dot3 = document.querySelectorAll(".dot3")
+let slides3 = dot3.length
+let cIndex3 = 0
 
 function slidePage1(index) {
     slideWrap1.style.transform = `translateX(-${index * 100}%)`
@@ -16,7 +22,13 @@ function slidePage1(index) {
             e.style.background = '#c4c4c4';
         }
     });
+    cIndex1 = index
 }
+
+setInterval(()=> {
+    cIndex1 = (cIndex1 + 1) % slides1
+    slidePage1(cIndex1)
+}, 5000)
 
 function slidePage2(index) {
     slideWrap2.style.transform = `translateX(-${index * 100}%)`
@@ -27,7 +39,13 @@ function slidePage2(index) {
             e.style.background = '#c4c4c4';
         }
     });
+    cIndex2 = index
 }
+
+setInterval(()=> {
+    cIndex2 = (cIndex2 + 1) % slides2
+    slidePage2(cIndex2)
+}, 5000)
 
 function slidePage3(index) {
     slideWrap3.style.transform = `translateX(-${index * 100}%)`
@@ -38,4 +56,10 @@ function slidePage3(index) {
             e.style.background = '#c4c4c4';
         }
     });
+    cIndex3 = index
 }
+
+setInterval(()=> {
+    cIndex3 = (cIndex3 + 1) % slides3
+    slidePage3(cIndex3)
+}, 5000)
