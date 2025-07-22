1f761a9d741664932b1e43b33d7852773f5435f7
parthsapariya745 <parthsapariyait7@gmail.com>
2025-07-22 23:37:36 +0530

    update

--- kidscard.js
+++ kidscard.js
@@ -1,115 +1,115 @@
 let kids = document.getElementById("kids")
 let kCard = [
   {
-    "id": 69,
-    "image": "https://levi.in/cdn/shop/files/182981482_01_Front_4b14f5bd-3c6a-4603-acda-096c5675d825.jpg?v=1740488440",
-    "company": "Levi's",
-    "product_name": "Men’s Slim Fit Jeans",
-    "discount_price": 1999
+    "id": 200,
+    "image": "https://www.jiomart.com/images/product/original/rvqp7keasx/pepp-n-hagg-boys-black-100-percent-cotton-t-shirt-and-shorts-set-6-years-7-years-product-images-rvqp7keasx-0-202307260920.jpg?im=Resize=(1000,1000)",
+    "company": "Hopscotch",
+    "product_name": "Boys Cotton T-Shirt & Shorts Set",
+    "discount_price": 599
   },
   {
-    "id": 70,
-    "image": "https://i.pinimg.com/736x/d5/ce/ec/d5ceecc69624ce256c62f568d7df0852.jpg",
-    "company": "H&M",
-    "product_name": "Men’s Casual Top",
+    "id": 201,
+    "image": "https://hawbeez.com/cdn/shop/files/Untitleddesign-2024-04-22T181120.448.png?v=1736327209",
+    "company": "Max",
+    "product_name": "Girls Printed Cotton Dress",
+    "discount_price": 749
+  },
+  {
+    "id": 202,
+    "image": "https://assets.ajio.com/medias/sys_master/root/20230628/yCjM/649b58eeeebac147fc10bc9b/-473Wx593H-465654966-grey-MODEL.jpg",
+    "company": "Babyoye",
+    "product_name": "Boys Dungaree Set with T-Shirt",
     "discount_price": 899
   },
   {
-    "id": 71,
-    "image": "https://m.media-amazon.com/images/I/518a424uw8L._UY1100_.jpg",
-    "company": "Nike",
-    "product_name": "Sports T-Shirt",
-    "discount_price": 799
-  },
-  {
-    "id": 72,
-    "image": "https://static.zara.net/assets/public/4619/aebc/4cfd4d43b103/baa249f7f934/06861441802-a1/06861441802-a1.jpg?ts=1727349036352",
-    "company": "Zara",
-    "product_name": "Formal Pant",
+    "id": 203,
+    "image": "https://m.media-amazon.com/images/I/91QLKUwxlKL._UY1100_.jpg",
+    "company": "Cucumber",
+    "product_name": "Girls Night Suit Combo",
     "discount_price": 699
   },
   {
-    "id": 73,
-    "image": "https://images-cdn.ubuy.co.in/654e5911b072f645b77dd5ff-puma-men-39-s-cover-french-terry.jpg",
-    "company": "Puma",
-    "product_name": "Joggers",
-    "discount_price": 1599
+    "id": 204,
+    "image": "https://deekayclothing.in/wp-content/uploads/2024/07/DSC00057-Edit-scaled.jpg",
+    "company": "Babyhug",
+    "product_name": "Boys Casual Shirt and Shorts",
+    "discount_price": 799
   },
   {
-    "id": 74,
-    "image": "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/bd46800b786548e7b4a1234553d66533_9366/Own_the_Run_3-Stripes_Shorts_Black_IQ3814_HM4.jpg",
-    "company": "Adidas",
-    "product_name": "Running Shorts",
+    "id": 205,
+    "image": "https://purecloth.co/cdn/shop/products/organic-cotton-romper-infant-girl_6_c629bfed-a148-4066-bd58-ff3ee2abd792.png?v=1654152835",
+    "company": "Nino Bambino",
+    "product_name": "Girls Organic Cotton Romper",
+    "discount_price": 499
+  },
+  {
+    "id": 206,
+    "image": "https://trenz.co.in/cdn/shop/products/skate-all-day-hoodie-sweatshirt-kids-448007.jpg?v=1705656085",
+    "company": "Cloth Theory",
+    "product_name": "Unisex Hooded Sweatshirt",
+    "discount_price": 649
+  },
+  {
+    "id": 207,
+    "image": "https://m.media-amazon.com/images/I/81NgRtHYouL._UY1100_.jpg",
+    "company": "Little Kangaroos",
+    "product_name": "Girls Denim Dungaree",
+    "discount_price": 999
+  },
+  {
+    "id": 208,
+    "image": "https://www.mashup.in/cdn/shop/products/M-5637_Blue.jpg?v=1594536846",
+    "company": "Pantaloons",
+    "product_name": "Boys Checked Shirt",
+    "discount_price": 549
+  },
+  {
+    "id": 209,
+    "image": "https://img.perniaspopupshop.com/catalog/product/l/i/LILIB112454_1.jpg?impolicy=detailimageprod",
+    "company": "Lilliput",
+    "product_name": "Girls Floral Printed Dress",
+    "discount_price": 849
+  },
+  {
+    "id": 210,
+    "image": "https://m.media-amazon.com/images/I/71+er7Q828L._UY1100_.jpg",
+    "company": "FirstCry",
+    "product_name": "Unisex Full Sleeve Nightwear",
+    "discount_price": 699
+  },
+  {
+    "id": 211,
+    "image": "https://www.momshome.in/cdn/shop/files/13_80f56c3e-7d12-4a5b-959e-3b686eeb124e.webp?v=1726928082",
+    "company": "Carter's",
+    "product_name": "Baby Bodysuit Combo (Pack of 5)",
     "discount_price": 1099
   },
   {
-    "id": 75,
-    "image": "https://m.media-amazon.com/images/I/61EvSi-Q7wL._UY1100_.jpg",
-    "company": "Uniqlo",
-    "product_name": "Lightweight Jacket",
-    "discount_price": 2799
+    "id": 212,
+    "image": "https://5.imimg.com/data5/SELLER/Default/2021/12/WV/QD/JL/27848514/white-tutu.jpg",
+    "company": "Moms Love",
+    "product_name": "Girls Frock with Headband",
+    "discount_price": 749
   },
   {
-    "id": 76,
-    "image": "https://rukminim2.flixcart.com/image/850/1000/kflftzk0-0/shirt/q/7/s/40-10398369-roadster-original-imafwyez4s2ctghk.jpeg?q=90&crop=false",
-    "company": "Roadster",
-    "product_name": "Checked Casual Shirt",
-    "discount_price": 1199
+    "id": 213,
+    "image": "https://m.media-amazon.com/images/I/71iA6w8C8HL._UY1100_.jpg",
+    "company": "Kookie Kids",
+    "product_name": "Boys Sleeveless T-shirt & Shorts",
+    "discount_price": 599
   },
   {
-    "id": 77,
-    "image": "https://imagescdn.vanheusenindia.com/img/app/product/3/39624826-12861529.jpg?auto=format&w=390",
-    "company": "Van Heusen",
-    "product_name": "Formal Blazer",
-    "discount_price": 4499
-  },
-  {
-    "id": 78,
-    "image": "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/19065302/2022/12/16/52f6c71a-9187-41ab-ac88-b6795fcf82f41671180852490-Anouk-Men-Kurtas-8111671180851957-1.jpg",
-    "company": "Biba",
-    "product_name": "Printed Kurta",
-    "discount_price": 1399
-  },
-  {
-    "id": 79,
-    "image": "https://m.media-amazon.com/images/I/A1rHT-qVzrL._AC_UY1100_.jpg",
-    "company": "Max",
-    "product_name": "Floral Dress",
-    "discount_price": 1599
-  },
-  {
-    "id": 80,
-    "image": "https://img.tatacliq.com/images/i8/1348Wx2000H/MP000000014796445_1348Wx2000H_202210090146491.jpeg",
-    "company": "Allen Solly",
-    "product_name": "Chino Trousers",
-    "discount_price": 1899
-  },
-  {
-    "id": 81,
-    "image": "https://rukminim2.flixcart.com/image/850/1000/kawtvgw0/track-pant/h/8/6/xxl-11012148-hrx-by-hrithik-roshan-original-imafsdhdwey2g2hu.jpeg?q=90&crop=false",
-    "company": "HRX",
-    "product_name": "Track Pants",
+    "id": 214,
+    "image": "https://staranddaisy.in/wp-content/uploads/2022/04/8812bbnnmm.jpg",
+    "company": "Mini Klub",
+    "product_name": "Girls Party Wear Dress",
     "discount_price": 1299
-  },
-  {
-    "id": 82,
-    "image": "https://spykar.com/cdn/shop/products/kbTYEA_5wy-MDSS1BC015MID-BLUE-_1.jpg?v=1750331187",
-    "company": "Spykar",
-    "product_name": "Denim Jeans",
-    "discount_price": 1799
-  },
-  {
-    "id": 83,
-    "image": "https://m.media-amazon.com/images/I/61Y0AlhowkL._UY1100_.jpg",
-    "company": "ONLY",
-    "product_name": "Lace Blouse",
-    "discount_price": 1499
   }
 ]
 
 kCard.forEach((e)=>{
     let dPersentage = (((Math.round(e.discount_price * 1.6) - e.discount_price) / Math.round(e.discount_price * 1.6)) * 100).toFixed(2)
-    box.innerHTML += `
+    kids.innerHTML += `
     <div class="shadow-lg overflow-hidden bg-white group relative rounded-xl transition-transform duration-300 hover:scale-105 hover:shadow-2xl animate-fadeInUp"
     onmouseenter="startSlider(this)" onmouseleave="stopSlider(this)">
       <div class="relative w-full aspect-[3/4] bg-gray-100 overflow-hidden">
